848c044dbbfb2c3424558707f2733a066e415714
afpa <afpa@POR7302295.exchange.ad.afpanet>
2021-02-01 15:52:14 +0100

    Excercices flux partie 2

---
+++ ExoPartie2Flux/src/main/java/fr/formation/afpa/App.java
@@ -0,0 +1,18 @@
+package fr.formation.afpa;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private final static Log log =LogFactory.getLog(App.class);
+
+    public static void main( String[] args )
+    {
+       log.debug("Log test");
+    }
+}
